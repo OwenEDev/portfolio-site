972879dd70719b1e6b0d772c1dc90f6971636c98
Owen <owen0879@gmail.com>
2024-12-12 02:14:51 +0000

    added text to make link

--- components/tabs.tsx
+++ components/tabs.tsx
@@ -5,13 +5,16 @@ const TabComponent = () => {
     <div className="w-full">
         <Tabs variant={"light"} color="primary">
             <Tab title={<h2 className="text-text">About Me</h2>}>
-                <div className="bg-primary w-full h-[250px] rounded-lg flex flex-col items-center p-10">
+                <div className="bg-primary w-full rounded-lg flex flex-col items-center p-7">
                     <h1 className="text-4xl font-semibold">Hello, I'm Owen</h1>
                     <h2 className="text-center p-5 font-semibold">
                         I am a software developer based in Manchester, UK.
                         I have used a variety of front-end technologies, specialising in React, and plenty of back-end and database technology too,
                         such as the AWS suite, SQL, and python.
                     </h2>
+                    <h1 className="p-5">
+                        View my CV
+                    </h1>
                 </div>
             </Tab>
             <Tab title={<h2 className="text-text">My Portfolio</h2>}>
